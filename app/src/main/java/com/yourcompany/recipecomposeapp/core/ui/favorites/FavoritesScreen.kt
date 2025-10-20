41805f008895e2cf50fa647c82ab4615ad117ca2
Maxim_Slushkin <slushkin.makcum@yandex.ru>
2025-10-20 10:38:47 +0300

    RCA-10: 1. Создан переиспользуемый ScreenHeader.kt 2. Создать экран 'Избранное'. 3. Создана заглушку экрана 'Рецепты'. 4. Обновлена навигация в RecipesApp.kt

--- app/src/main/java/com/yourcompany/recipecomposeapp/core/ui/favorites/FavoritesScreen.kt
+++ app/src/main/java/com/yourcompany/recipecomposeapp/core/ui/favorites/FavoritesScreen.kt
@@ -1,46 +1,48 @@
-package com.yourcompany.recipecomposeapp.core.ui.categories
+package com.yourcompany.recipecomposeapp.core.ui.favorites
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.yourcompany.recipecomposeapp.R
 import com.yourcompany.recipecomposeapp.core.ui.ScreenHeader
 
 @Composable
-fun CategoriesScreen(modifier: Modifier = Modifier) {
+fun FavoritesScreen(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier.fillMaxSize()
     ) {
         ScreenHeader(
-            header = "Категории",
-            imageRes = R.drawable.bcg_categories,
-            modifier = Modifier,
+            header = "Избранное",
+            imageRes = R.drawable.bcg_favorites,
+            modifier = Modifier
         )
-        CategoriesContent()
+        FavoritesContent()
     }
 }
 
 @Composable
-fun CategoriesContent() {
+fun FavoritesContent() {
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.background),
         contentAlignment = Alignment.Center
     ) {
-        Text("Здесь будут категории")
+        Text("Здесь будут избранные рецепты")
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun CategoriesScreenPreview() {
-    CategoriesScreen()
+fun FavoritesScreenPreview() {
+    FavoritesScreen()
 }
