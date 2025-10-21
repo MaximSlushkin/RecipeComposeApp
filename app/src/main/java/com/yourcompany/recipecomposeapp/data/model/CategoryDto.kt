fd91429ae418935cbc2aee4074be93638a1e2821
Maxim_Slushkin <slushkin.makcum@yandex.ru>
2025-10-21 20:37:49 +0300

    Revert "RCA-11:"
    This reverts commit f94df4cbc09bef6c5f64dc07805de4aa9ff0c51b.

--- app/src/main/java/com/yourcompany/recipecomposeapp/data/model/CategoryDto.kt
+++
@@ -1,8 +0,0 @@
-package com.yourcompany.recipecomposeapp.data.model
-
-data class CategoryDto(
-    val id: Int,
-    val title: String,
-    val description: String,
-    val imageUrl: String,
-)
--- app/src/main/java/com/yourcompany/recipecomposeapp/data/model/IngredientDto.kt
+++
@@ -1,7 +0,0 @@
-package com.yourcompany.recipecomposeapp.data.model
-
-data class IngredientDto(
-    val quantity: String,
-    val unitOfMeasure: String,
-    val description: String,
-)
