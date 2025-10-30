d4218a0f7ab4f5ff883adc0c2240fd4870089f62
Maxim_Slushkin <slushkin.makcum@yandex.ru>
2025-10-30 19:32:39 +0300

    RCA-14: 1. Подключил библиотеку Coil для загрузки изображений 2. Создал компонент CategoryItem в ui/categories/ 3. Реализовал CategoriesScreen в ui/categories/

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.ui)
+    implementation(libs.androidx.compose.runtime)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -63,4 +64,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
 }
