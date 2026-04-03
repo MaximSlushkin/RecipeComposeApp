81054c31350befb17a8afbbc0c8840dbc957742b
Maxim_Slushkin <slushkin.makcum@yandex.ru>
2026-04-03 20:08:01 +0300

    RCA-50 changes: В CategoriesContentTest.kt и RecipesContentTest.kt добавлены @RunWith(AndroidJUnit4::class) над объявлением класса — без него instrumented тест не запустится на устройстве

--- app/src/androidTest/java/com/yourcompany/recipecomposeapp/features/categories/ui/CategoriesContentTest.kt
+++ app/src/androidTest/java/com/yourcompany/recipecomposeapp/features/categories/ui/CategoriesContentTest.kt
@@ -5,12 +5,15 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.yourcompany.recipecomposeapp.features.categories.presentation.model.CategoriesUiState
 import com.yourcompany.recipecomposeapp.features.categories.presentation.model.CategoryUiModel
 import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class CategoriesContentTest {
 
     @get:Rule
--- app/src/androidTest/java/com/yourcompany/recipecomposeapp/features/recipes/ui/RecipesContentTest.kt
+++ app/src/androidTest/java/com/yourcompany/recipecomposeapp/features/recipes/ui/RecipesContentTest.kt
@@ -5,13 +5,16 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.yourcompany.recipecomposeapp.core.ui.components.ingredients.presentation.model.IngredientUiModel
 import com.yourcompany.recipecomposeapp.features.recipes.presentation.model.RecipesUiState
 import com.yourcompany.recipecomposeapp.features.recipes.presentation.model.RecipeUiModel
 import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class RecipesContentTest {
 
     @get:Rule
